a6a310bacccf82e4ba9e5761c7ab54b5c2628a86
Jyotsna <jagjyotsna@gmail.com>
2018-03-18 00:12:54 +0530

    changed onKeyUp evevt to onKeyPressed

--- app.js
+++ app.js
@@ -2,19 +2,29 @@ var count = 0
 
 function KeyUpFunction(e) {
    	var enteredKey = e.key
-   	var expectedKey = document.getElementById('typingArea').textContent[count]
-   	var output = document.getElementById("output")
-   	var span = document.createElement('span')
 
-   	span.style.color = "black"
-      span.style.background = "red"
-   	if (enteredKey == expectedKey) {
-   		span.style.color = "#33ff39"
-         span.style.background = "none"
-   	}
+      console.log("*********")
+      console.log(enteredKey)
+      console.log(enteredKey.charCodeAt(0))
+      var asc = enteredKey.charCodeAt(0)
 
-   	span.innerHTML = enteredKey
-   	output.appendChild(span)
+      if (asc < 122 && asc >= 32)
+      {
 
-   	count = count + 1
+      	var expectedKey = document.getElementById('typingArea').textContent[count]
+      	var output = document.getElementById("output")
+      	var span = document.createElement('span')
+
+      	span.style.color = "black"
+         span.style.background = "red"
+      	if (enteredKey == expectedKey) {
+      		span.style.color = "#33ff39"
+            span.style.background = "none"
+      	}
+
+      	span.innerHTML = enteredKey
+      	output.appendChild(span)
+
+      	count = count + 1
+      }
 }
